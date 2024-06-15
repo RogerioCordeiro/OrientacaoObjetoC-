f15e9f9b80184fc37cddd4f2ebfd93aaa944e5ec
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-15 17:05:48 -0300

    Exercício de pratico para reforçar o conteudo aprendido até o momento.

---
+++ ExercicioEscolar/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioEscolar
+{
+    internal class Aluno
+    {
+        // Atributos
+        public string? nome;
+        public double nota1, nota2;
+
+        // Média
+        public double Media()
+        {
+            return (nota1 + nota2) / 2;
+        }
+        // Situação
+        public static string Situacao(double media)
+        {
+            return media >= 7 ? "aprovado" : "reprovado";
+        }
+
+        // Mensagem
+        public void Mensagem()
+        {
+            // Obter a média
+            double obterMedia = Media();
+            // Obter a situacao
+            string obterSituacao = Situacao(obterMedia);
+            // Mensagem
+            Console.WriteLine($"{nome} está {obterSituacao} com média {obterMedia}!");
+
+        }
+    }
+}
---
+++ ExercicioEscolar/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExercicioEscolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar objeto da classe
+            Aluno aluno1 = new Aluno();
+            aluno1.nome = "Robson";
+            aluno1.nota1 = 5;
+            aluno1.nota2 = 3;
+            aluno1.Mensagem();
+
+        }
+    }
+}

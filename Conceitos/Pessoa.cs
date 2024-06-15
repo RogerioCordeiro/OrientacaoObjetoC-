e86e06ca459cb866017a36c77c527f32664eb7a0
Rogerio <Rogerio@MACK2>
2024-06-15 16:17:45 -0300

    Primeira aula, incluindo conceitos de criação da classe, seus atributos e métodos.

---
+++ Conceitos/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conceitos
+{
+    internal class Pessoa
+    {
+        // Atributos
+        public string nome;
+        public int idade;
+
+        // Métodos
+        public void mensagem()
+        {
+            Console.WriteLine("Olá " + nome + " você tem " + idade + " anos");
+        }
+    }
+}
---
+++ Conceitos/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Conceitos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciar um objeto
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.nome = "Robson";
+            pessoa1.idade = 30;
+            pessoa1.mensagem();
+        }
+    }
+}

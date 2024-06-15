a73f20fdb56989558b6fb67884c71f24a11c75f0
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-15 16:42:02 -0300

    Métodos, forma de declarar os métodos onde pode ser utilizados os mesmos nomes nos desde que os parametros de cada débito sejam diferentes.

---
+++ Metodos/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Pessoa
+    {
+        // Método 01
+        public void apresentar()
+        {
+            Console.WriteLine("Olá!!!");
+        }
+        // Método 02
+        public void apresentar(string nome)
+        {
+            Console.WriteLine($"Olá {nome}!");
+        }
+        // Método 03
+        public void apresentar(string nome, int idade)
+        {
+            Console.WriteLine($"Olá {nome} voê tem {idade} anos.");
+        }
+    }
+}
---
+++ Metodos/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.apresentar();
+        }
+    }
+}

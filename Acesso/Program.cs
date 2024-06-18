902b0eb49c4eadbe5f590fc8423a7e792f973850
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-17 21:52:54 -0300

    Codigo que mostra como funciona os acesso public, private, protected em classes na orientaçao a objeto.

---
+++ Acesso/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Acesso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto();
+            produto1.nome = "Tablet";
+            Console.WriteLine(produto1.nome);
+        }
+    }
+}
